bf80935bcb8c706ea01229c2f1d36c9461894956
Venkat <subburavulapalli076@gmail.com>
2022-06-21 19:40:37 +0530

    new changes added

--- ass9.py
+++ ass9.py
@@ -1,4 +1,257 @@
-a=10
-b=20
-c=a+b
-print(c)
+# a=10
+# b=20
+# c=a+b
+# print(c)#x=123
+# #print(float(x))
+# #print(bool(x))
+# #print(str(x))
+# #print(complex(x))
+# #x=[1,2,3,4,4,5]
+# #
+# #list=x
+# #x[0]=100
+# #x[3]=100
+# #print(x)
+# #x="hello world"
+# #print(len(x))
+# #x="subbu"
+# #print(float(x))
+# #a=10
+# #b=-20
+# #c=a-b
+# #print(c)
+# #x=range(11)
+# #print(x)
+# #for i in x:
+#  # print(i,end="  ")
+# #x="HELLO PYTHON"
+# #print(x[2:len(x)-7])
+# #x=[1,2,3,4,5,3,5]
+# #x=set(x)
+# #print(list(x))
+# #x=[1,2,3]
+# #x=[1*10,2*10,3*10]
+# #print(x)
+# #x=range(0,20,2)
+# #for j in x:
+#  #print(j,end="   ")
+# #x = [1, 2, 3, 4, 5]
+# #print(x[0:5:2])
+# #x=[1,2,3]
+# #print(x[::-1])
+# #a=10
+# #b=20
+# #a=a+b
+# #b=a-b
+# #a=a-b
+# #print("a value is",a)
+# #print("b value is",b)
+# #x=(1,2,3,4)
+# #x=dict
+# #print(type(x))
+# #print(x)
+# #x=9
+# #print(bin(x))
+#
+#
+# #x=[1,2,3,4,5]
+# #[0]=1**3
+# #x[1]=2**3
+# #x[2]=3**3
+# #x[3]=4**3
+# #x[4]=5**3
+# #print(x)
+# #x={"a":20,"b":25}
+# #print(x.keys())
+# #x={"subbu"}
+# #y={'A':1,'B':124}
+# #print(type(x))
+# #print(type(y))
+# # x,y=10,20
+# # print(x,y)
+# # x=10
+# # y=20
+# # print(x<=10 and y<=0)
+# # print(x>=y or y>=x)
+# # print(x==0 or y==0)
+# # print(x<10 or y>10
+# # x="$"
+# # print("ascii value is ",(ord(x)))
+# # x=3
+# # print(pow(x,3))
+# # x=[1,2,3,1,2,3]
+# # x=set(x)
+# # print(type(x))
+# # print(x)
+# # # x=[1,2,3,1,2,3]
+# # x=tuple(x)
+# # print(x)
+# # print(type(x))
+# # x=[1,2,3,4,4,5]
+# # print(x[1:-1])
+# # x=dict()
+# # print(type(x))
+# # x=123
+# # x=dict()
+# # # print(type(x))
+# # x={123}
+# # print(type(x))
+# # x,y=10,15
+# # print(x,y)
+# # c=y if x>=y else x
+# # # print(c)
+# # a,b = 1,20
+# # print(a and b)
+# # a=25
+# # b=5
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a/b)
+# # print(a%b)
+# # a=25
+# # b=5
+# # print(a>b)
+# # print(a<b)
+# # print(a>=b)
+# # print(a<=b)
+# # a = "python"
+# # print(a * 3)
+# # a = ' '
+# # b = 3
+# # c=a*b
+# # print(len(c))
+# # import math
+# # a=123.5
+# # b=123
+# # a=+b
+# # print(a)
+# # x=range(11)
+# # for i in x:
+# #  print(i,end=" ")
+# # y=
+# # print(y)
+# # x=[1,2,3,4,5,6,7,8,9,10]
+# # print(x[::-1])
+# #
+# #a=1
+# # b=20
+# # print(a and b)
+#
+# #a=1
+# # b=2
+# # a=a^b
+# # b=a^b
+# # a=a^b
+# # print(a,b)
+# # a=123
+# # print(bin(a))
+# # print(str(a))
+# # x=[1,2,3,4]
+# # x[0]=pow(x[0],3)
+# # x[1]=pow(x[1],3)
+# # x[2]=pow(x[2],3)
+# # x[3]=pow(x[3],3)
+# # print
+# # x=list(range(1,11))
+# # y=list(x)
+# # print(y[1::2])
+# # x=[1,2,3,4,3]
+# # print(x[::-1])
+# # a=2
+# # b=3
+# # print(a<<b)
+# # x = "HELLO"
+# # print(x[1:-2])
+# #
+# # x="subbu"
+# # y="ram"
+# # print(x+y)
+# # x=10
+# # y=20
+# # if x<y:
+# #     print("biggest value is",x)
+# # given no. is even or not
+# # a=int(input("enter a value :"))
+# # if a%2==0:
+# #     print("the number is even " ,a)
+# # a=20
+# # b=25
+# # if a%2==0 and a<b:
+# #     print("student")
+# # else:
+# #     print("teacher")
+# # a=int(input("enter a value:"))
+# # if a%2==0:;
+# #     print("the number is even")
+# # else:
+# #     print("the number is odd")
+# # a=5
+# # b=19
+# # if a<b or b>a:
+# #     print("python")
+# # elif a==0 or b>a:
+# #     print("data science")
+# # else:
+# # #     print("java")
+# # a=int(input("student marks:"))
+# # if a>90:
+# #     print("student got O")
+# #     if a>80:
+# #         print("student got A grade")
+# #         if a>70:
+# #             print("student got B grade")
+# #             if a>60:
+# #                 print("student got C grade")
+# #                 if a>50:
+# #                     print("student got D grade")
+# #                     if a<50:
+# #                         print("student fail")
+# # a=25
+# # b=225
+# # if a<b or b<67:
+# #     print("infosys")
+# #     if a%2 or b==225:
+# #         print("tata")
+# #     elif a<=30:f
+# #         print("wipro")
+# #     else:
+# #         print("cognizent")
+# # else:
+# #     print("facebook")
+# # n=int(input("given number is:"))
+# # if n%2==0:
+# #     print("the num is even")
+# # else:
+# #     print("the num is odd")
+# # n=[1,2,3,4,5,8,7,0]
+# # print(n[::-1])
+# # x=[10,20,5,1]
+# # for i  in range(len(x)):
+# #     print(x[i],end=" ")
+# # n=int(input("enter the num:"))
+# # f=1
+# # for i in range(1,n+1):
+# #     f=f*i
+# # #     print("fact of %d is %d" %(n,f))
+# # x=list(range(1,11))
+# # for i in x:
+# #     print(i,end=" ")
+# # n = 1234
+# # r= 0
+# # while n!= 0:
+# #     temp= n%10
+# #     r= r*10 + temp
+# #     n//= 10
+# #
+# # print("Reversed Number: " + str(r,temp))
+# # n=int(input("enter a val:"))
+# # s=0
+# # temp=n
+# # while(n>0):
+# #     r=n%10
+# #     a=r*10+r
+# #     n=n//10
+# #     print("reverse num of %d id: %d"  %(temp,s)
+#
+# ###adding to git
